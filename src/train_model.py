e7d1369c0a0507289f60341758255cc599d6f812
Amit Chakraborty <35186776+sysadminamit@users.noreply.github.com>
2022-01-28 20:15:31 +0530

    reduce dense layer

--- src/train_model.py
+++ src/train_model.py
@@ -36,8 +36,8 @@ def model_def():
 
     # Add the fully-connected layers
     x = Flatten(name='flatten')(output_vgg16_conv)
-    x = Dense(4096, activation='relu', name='fc1')(x)
-    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Dense(2096, activation='relu', name='fc1')(x)
+    x = Dense(2096, activation='relu', name='fc2')(x)
     x = Dense(num_classes, activation='softmax', name='predictions')(x)
 
     # Create your own model
